be9afbcf5a66b16d5bdfd12964cf842c31521287
Sean Padden <you@example.com>
2019-07-01 19:59:56 -0400

    CRU on both models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
+Student.destroy_all
+Duck.destroy_all
+
 10.times do
   Student.create(name: Faker::Name.name, mod: rand(1..5))
 end
 
 5.times do
-  Duck.create(name: Faker::Dog.meme_phrase, description: Faker::Dog.breed)
+  Duck.create(name: Faker::Dog.meme_phrase, description: Faker::Dog.breed, student: Student.all.sample)
 end
+
+puts "it's been seeded"
