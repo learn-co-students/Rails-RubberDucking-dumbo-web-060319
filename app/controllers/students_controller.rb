c66e18277ea23360f1da1b658b8743541737cd97
github username <github email address>
2019-07-02 16:24:40 -0400

    Full CRUD Associations

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,54 @@
 class StudentsController < ApplicationController
 
+  before_action :find_student, only: [:show, :edit, :destroy, :update]
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    if @student.valid?
+      redirect_to @student
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to new_student_path
+    end
+  end
+
   def index
     @students = Student.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @student.update(student_params)
+    if @student.valid?
+      redirect_to @student
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to @student
+    end
+  end
+
+  def destroy
+    @student.destroy
+    redirect_to students_path
+  end
+
+  private
+
+  def find_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
   end
 
 end
